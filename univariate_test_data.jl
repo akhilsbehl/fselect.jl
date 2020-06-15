d4e7169de5952558396ded657ebb756417972b54
akhilsbehl <akhilsbehl@gmail.com>
2020-06-15 17:04:01 -0400

    Refactor testing entirely.

--- univariate_test_data.jl
+++ univariate_test_data.jl
@@ -1,4 +1,5 @@
 using BenchmarkTools
+using CSV
 using DataFrames
 using Distributions
 using PyCall
@@ -7,75 +8,70 @@ using VegaDatasets
 pyfs = pyimport("sklearn.feature_selection")
 pypp = pyimport("sklearn.preprocessing")
 
-# http://facultysites.vassar.edu/lowry/PDF/c14p1.pdf
-features =
-[
- [27.0, 26.2, 28.8, 33.5, 28.8],
- [22.8, 23.1, 27.7, 27.6, 24.0],
- [21.9, 23.4, 20.1, 27.8, 19.3],
- [23.5, 19.6, 23.7, 20.8, 23.9],
-]
-
 ########################################
 #  Collect some datasets to test with  #
 ########################################
 
-iris = dataset("iris") |> DataFrame
-seattle = dataset("seattle-weather") |> DataFrame
-wind = dataset("windvectors") |> DataFrame
-zip = dataset("zipcodes") |> DataFrame
+class_datasets = ["iris", "seattle-weather", "windvectors", "zipcodes"]
+# cd := class_datasets
+cd_splits = [[1:4, 5], [2:5, 6], [[1, 2, 5], 4], [2:3, 5]]
 
-class_datasets =
-Dict(
-     :iris => Dict(
-                   :X => Matrix(iris[:, 1:4]),
-                   :y => Array(iris[!, 5]),
-                  ),
-     :seattle => Dict(
-                      :X => Matrix(seattle[:, 2:5]),
-                      :y => Array(seattle[!, 6]),
-                     ),
-     :wind => Dict(
-                   :X => Matrix(wind[:, [1, 2, 5]]),
-                   :y => Array(wind[!, 4]),
-                  ),
-     :zip => Dict(
-                  :X => Matrix(zip[:, 2:3]),
-                  :y => Array(zip[!, 5]),
-                 ),
-    )
+class_data = Dict()
+for (dname, dsplit) in Iterators.zip(class_datasets, cd_splits)
+  df = DataFrame(dataset(dname))
+  X, y = Matrix(df[!, dsplit[1]]), df[!, dsplit[2]]
+  class_data[dname] = Dict(:X => X, :y => y)
+end
 
-tests_metadata =
+# https://github.com/vincentarelbundock/Rdatasets
+reg_datasets = ["freedman", "highway", "salaries"]
+# rd := reg_datasets
+rd_splits = [[2, 3], 4], [3:11, 2], [4:5, 7]]
+
+reg_data = Dict()
+for (dname, dsplit) in Iterators.zip(reg_datasets, cd_splits)
+  println(dname)
+  df = DataFrame(CSV.read("data/$(dname).csv"))
+  X, y = Matrix(df[!, dsplit[1]]), df[!, dsplit[2]]
+  reg_data[dname] = Dict(:X => X, :y => y)
+end
+
+tests_functions =
 Dict(
-     :ftest => quote
-       all(isapprox.(ftest_classification(pair[:X], pair[:y]),
-                     pyfs.f_classif(pair[:X], pair[:y])))
-     end,
-     :binarize_classes => quote
-       binarize_classes(pair[:y]) ==
-       pypp.LabelBinarizer().fit_transform(pair[:y])
-     end,
-     :chisq => quote
-       all(isapprox.(chisq(abs.(pair[:X]), pair[:y]),
-                     pyfs.chi2(abs.(pair[:X]), pair[:y])))
-     end,
+     :ftest_classification =>
+     (X, y) -> all(isapprox.(ftest_classification(X, y),
+                             pyfs.f_classif(X, y))),
+     :binarize_classes =>
+     (X, y) -> (binarize_classes(y) == pypp.LabelBinarizer().fit_transform(y)),
+     :chisq =>
+     (X, y) -> all(isapprox.(chisq(abs.(X), y), pyfs.chi2(abs.(X), y))),
     )
 
 ############################
 #  Let the testing begin!  #
 ############################
 
-statistic, prob = (6.423139965384153, 0.004622990952613463)
-@assert one_way_anova(features...) == (statistic, prob)
-
-for (data_label, dataset) in class_datasets
+for (data_label, dataset) in class_data
   println()
   println("============================================================")
-  println("Testing for data_label: $(data_label)")
-  for (funcn, expr) in tests_metadata
-    global pair = dataset
-    println("""$(funcn): $(eval(expr) ? "PASSED" : "FAILED")""")
+  println("Testing for data set: $(data_label)")
+  for (test_name, test_func) in tests_functions
+    check = test_func(dataset[:X], dataset[:y])
+    println("""$(test_name): $(check ? "PASSED" : "FAILED")""")
   end
-  println("Finished for data_label: $(data_label)")
+  println("Finished for data set: $(data_label)")
   println("============================================================")
 end
+
+for (data_label, dataset) in reg_data
+  println()
+  println("============================================================")
+  println("Testing for data set: $(data_label)")
+  for (test_name, test_func) in tests_functions
+    check = test_func(dataset[:X], dataset[:y])
+    println("""$(test_name): $(check ? "PASSED" : "FAILED")""")
+  end
+  println("Finished for data set: $(data_label)")
+  println("============================================================")
+end
+
